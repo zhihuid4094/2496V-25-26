d90e230bbe7a3a8e1a7e3109032d1a9231b67308
zhihuid4094 <athenad0604@gmail.com>
2025-08-13 21:49:02 -0700

    i forgot to save the pid code

---
+++ src/pidtest.cpp
@@ -0,0 +1,16 @@
+#include "api.h"
+#include "main.h"
+#include "pid.h"
+#include "robot.h"
+#include "auton.h"
+#include "odometry.h"
+
+PID::PID(float error, float kp, float kd, float starti):
+error(error),
+kp(kp),
+ki(ki),
+kd(kd),
+starti(starti)
+{};
+
+
